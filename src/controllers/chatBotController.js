5f01bb2fce8f3fbc6851cdc0fda924d2422b98e6
mendelsun111 <mendelsun21@gmail.com>
2020-06-23 12:01:43 -0400

    app setting up test message

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -23,7 +23,22 @@ let postWebhook = (req, res) =>{
   // Get the sender PSID
   let sender_psid = webhook_event.sender.id;
   console.log('Sender PSID: ' + sender_psid);
-      
+      // Gets the body of the webhook event
+  let webhook_event = entry.messaging[0];
+  console.log(webhook_event);
+
+
+  // Get the sender PSID
+  let sender_psid = webhook_event.sender.id;
+  console.log('Sender PSID: ' + sender_psid);
+
+  // Check if the event is a message or postback and
+  // pass the event to the appropriate handler function
+  if (webhook_event.message) {
+    handleMessage(sender_psid, webhook_event.message);        
+  } else if (webhook_event.postback) {
+    handlePostback(sender_psid, webhook_event.postback);
+  }
     });
 
     // Return a '200 OK' response to all events
