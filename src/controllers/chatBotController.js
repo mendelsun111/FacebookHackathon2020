7fb7b8ed2d4ba08318e306709e72789a5e027f1e
mendelsun111 <mendelsun21@gmail.com>
2020-06-23 12:41:49 -0400

    attempt to remove echo

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -82,7 +82,8 @@ function handleMessage(sender_psid, received_message) {
 
     // Create the payload for a basic text message
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`,
+      "is_echo": false
     }
   } else if (received_message.attachments) {
   
