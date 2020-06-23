70f9ae5201cbfdec5b67c3e733c9661ced3702f0
mendelsun111 <mendelsun21@gmail.com>
2020-06-23 01:26:02 -0400

    update webhook

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,14 @@
 import express from "express";
 import homepageController from "../controllers/homepageController"
+import chatBotController from "../controllers/chatbotController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homepageController.getHomePage);
-
+    router.get("/webhook", chatBotController.getWebhook);
+    router.post("/webhook", chatBotController.postWebhook);
+    
     return app.use("/", router);
 };
 
