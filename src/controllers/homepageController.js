70d881a9cabe830cc79cc29fa9b903c6ecafbbbe
mendelsun111 <mendelsun21@gmail.com>
2020-06-23 18:22:33 -0400

    setting up get started

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -1,7 +1,7 @@
   
 // import homepageService from "../services/homepageService";
 
-import request from "request";
+//import request from "request";
 require("dotenv").config();
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
@@ -78,7 +78,6 @@ let setUpUserFacebookProfile = async (req, res) => {
         })
     }
 };
-
 
 
 module.exports = {
--- src/routes/web.js
+++ src/routes/web.js
@@ -5,7 +5,7 @@ import chatBotController from "../controllers/chatBotController";
 let router = express.Router();
 
 let initWebRoutes = (app) => {
-    router.get("/", homepageController.getHomepage);
+    router.get("/", homepageController.getHomePage);
     router.get("/webhook", chatBotController.getWebhook);
     router.post("/webhook", chatBotController.postWebhook);
     router.get("/profile", homepageController.getFacebookUserProfile);
