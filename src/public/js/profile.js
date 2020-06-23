972fc89311324c39eb9eed4b2771d2973d1690aa
mendelsun111 <mendelsun21@gmail.com>
2020-06-23 19:12:37 -0400

    setting up get started

--- src/public/js/profile.js
+++ src/public/js/profile.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     $("#submitFacebookProfile").on("click", function(event) {
         event.preventDefault();
+        
         $.ajax({
             url: `${window.location.origin}/set-up-user-fb-profile`,
             method: "POST",
             data: {},
             success: function(data) {
                 alert("Setup succeeds");
-                console.log(data);
+                console.log(data); 
             },
             error: function(error) {
                 console.log(error);
