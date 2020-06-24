e68d04ae841d8d48f9c25eea3a89545e72f8bdd3
xdeng10 <60670592+xdeng10@users.noreply.github.com>
2020-06-24 10:54:31 -0400

    Create handleMessageWithEntities and firstEntity function and update handleMEssage function

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -77,50 +77,10 @@ function handleMessage(sender_psid, received_message) {
   //Handle text message
   handleMessageWithEntities(received_message);
 
+  //handle quick reply message
 
-  // //NEW CODE
-  // let response;
+  //handle attachment message
 
-  // // Check if the message contains text
-  // if (received_message.text) {
-
-  //   // Create the payload for a basic text message
-  //   response = {
-  //     "text": `You sent the message: "${received_message.text}". Now send me an image!`
-  //   }
-  // } else if (received_message.attachments) {
-
-  //   // Gets the URL of the message attachment
-  //   let attachment_url = received_message.attachments[0].payload.url;
-  //   response = {
-  //     "attachment": {
-  //       "type": "template",
-  //       "payload": {
-  //         "template_type": "generic",
-  //         "elements": [{
-  //           "title": "Is this the right picture?",
-  //           "subtitle": "Tap a button to answer.",
-  //           "image_url": attachment_url,
-  //           "buttons": [
-  //             {
-  //               "type": "postback",
-  //               "title": "Yes!",
-  //               "payload": "yes",
-  //             },
-  //             {
-  //               "type": "postback",
-  //               "title": "No!",
-  //               "payload": "no",
-  //             }
-  //           ],
-  //         }]
-  //       }
-  //     }
-  //   }
-  // }
-
-  // // Sends the response message
-  // callSendAPI(sender_psid, response);
 }
 
 //Use Facebook AI to read user message
